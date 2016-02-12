0615bf4f2b75d09bd7bd428b5161900cce7c90a9
abhagwt2 <abhagwt2@0c76c217-e028-4356-9bb7-d03e9a460cca>
2016-02-12 22:15:41 +0000

    Final Submission

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -8,9 +8,10 @@ int main()
 	printf("Enter n, omega1, omega2:");
 	scanf("%d %lf %lf" ,&n,&omega1,&omega2);
 	int i;
-	for(i=0, i<n, i++)
+	for(i=0; i<n; i++)
 	{
 		x=(i*3.14)/n;
 		f=sin(omega1*x)+0.5*sin(omega2*x);
 		printf("%lf %lf\n", x, f);
 	}
+}
