f2b9e9eb8bf401598a1a8a9b90c6ff613022ea94
abhagwt2 <abhagwt2@0c76c217-e028-4356-9bb7-d03e9a460cca>
2016-03-05 23:04:11 +0000

    First Draft

--- mp6/functions.c
+++ mp6/functions.c
@@ -15,7 +15,30 @@
  */
 void calculate_cosine_filter(double *cos_filter,int radius)
 {
-    //YOUR CODE HERE
+	int i, j, k; 
+	double x, y;
+	double weight = 0.0;
+	int length = 2*radius + 1;
+	
+	for(i = 0; i < length; i++)
+	{
+		for(j = 0; j < length; j++)
+		{
+			x = j - radius;
+			y = i - radius;
+			cos_filter[length*i + j] = cos((M_PI*x)/(2*radius)) + cos((M_PI*y)/(2*radius));
+		}
+	}
+
+	for(k = 0; k < length*length; k++)
+	{
+		weight = weight + cos_filter[k];		
+	}
+
+	for(k = 0; k < length*length; k++)
+	{
+		cos_filter[k] = cos_filter[k]/weight;
+	}	 		
 }
 
 /* convolve_image - performs a convolution between a filter and image
@@ -42,7 +65,58 @@ void convolve_image(uint8_t *in_red,uint8_t *in_green,uint8_t *in_blue,
                    uint8_t *out_blue,uint8_t *out_alpha,const double *filter,
                    int radius,int width,int height)
 {
-    //YOUR CODE HERE
+	int a, b, c, d, i, j, k, x, y, area ,length;
+	double in_sum_red, out_sum_red, in_sum_green, out_sum_green, in_sum_blue, out_sum_blue;
+	in_sum_red = out_sum_red = in_sum_green = out_sum_green = in_sum_blue = out_sum_blue = 0;	
+	area = width*height;
+	length = 2*radius + 1;
+
+
+	if(radius < 1)
+	{
+		for(k = 0; k < area; k++)
+		{
+			out_red[k] = in_red[k];
+			out_green[k] = in_green[k];
+			out_blue[k] = in_blue[k];
+			out_alpha[k] = in_alpha[k];
+		}
+	}
+	
+	for(y = 0; y < height; y++)
+	{
+		for(x = 0; x < width; x++)
+		{
+			for(i = -radius; i < radius + 1; i++)
+			{
+				for(j = -radius; j < radius + 1; j++)
+				{
+					a = i + radius;
+					b = j + radius;
+					c = x + i;
+					d = y + j;
+
+					if(c < 0 || c > width-1 || d < 0 || d > height-1)
+					{
+						c = d = 0;
+					}
+					else
+					{
+						in_sum_red = in_sum_red + filter[b*length + a]*in_red[d*length + c];
+						in_sum_green = in_sum_green + filter[b*length + a]*in_red[d*length + c];
+						in_sum_blue = in_sum_blue + filter[b*length + a]*in_red[d*length + c];						
+					}
+				}
+				//out_sum_red = out_sum_red + in_sum_red;
+				//out_sum_green = out_sum_green + in_sum_green;
+				//out_sum_blue = out_sum_blue + in_sum_blue;
+			}
+			out_red[y*width + x] = in_sum_red;
+			out_green[y*width + x] = in_sum_green;
+			out_blue[y*width + x] = in_sum_blue;
+			out_alpha[y*width + x] = in_alpha[y*width + x];
+		}
+	}						
 }
 
 /* convert_to_gray - convert the input image to grayscale
@@ -69,7 +143,21 @@ void convert_to_gray(uint8_t *in_red,uint8_t *in_green,uint8_t *in_blue,
                    uint8_t *out_blue,uint8_t *out_alpha,
                    const double *gmonomult,int width,int height)
 {
-    //YOUR CODE HERE
+	int i, area;
+	double a, b, c, gray_num;
+
+	area = width*height;
+
+	for(i = 0; i < area; i++)
+	{
+		a = in_red[i]*gmonomult[0];
+		b = in_green[i]*gmonomult[1];
+		c = in_blue[i]*gmonomult[2];		
+		
+		gray_num = a + b + c;
+		out_red[i] = out_green[i] = out_blue[i] = gray_num;
+		out_alpha[i] = in_alpha[i];
+	}
 }
 
 /* flip_vertical- flips image vertically such that the bottom row of pixels
@@ -96,7 +184,18 @@ void flip_vertical(uint8_t *in_red,uint8_t *in_green,uint8_t *in_blue,
                  uint8_t *in_alpha,uint8_t *out_red,uint8_t *out_green,
                  uint8_t *out_blue,uint8_t *out_alpha,int width,int height)
 {
-    //YOUR CODE HERE
+	int i, j;
+	
+	for(i = 0; i < height; i++)
+	{
+		for(j = 0; j < width; j++)
+		{
+			out_red[width*i + j] = in_red[width*(height-1-i) + j];
+			out_green[width*i + j] = in_green[width*(height-1-i) + j];
+			out_blue[width*i + j] = in_blue[width*(height-1-i) + j];
+			out_alpha[width*i + j] = in_alpha[width*(height-1-i) + j];
+		}
+	}
 }
 
 /* color_threshold - for each pixel of input image, compares value to red, green, and blue threshold.
@@ -122,5 +221,39 @@ void color_threshold(uint8_t *in_red,uint8_t *in_green,uint8_t *in_blue,
                  uint8_t *out_blue,uint8_t *out_alpha,int width,int height,
                  int red_threshold,int blue_threshold,int green_threshold)
 {
-     //YOUR CODE HERE
+	int i, area;
+
+	area = width*height;
+
+	for(i = 0; i < area; i++)
+	{
+		if(in_red[i] > red_threshold)
+		{
+			if(in_green[i] > green_threshold)
+			{
+				if(in_blue[i] > blue_threshold)
+				{
+					out_red[i] = in_red[i];
+					out_green[i] = in_green[i];
+					out_blue[i] = in_blue[i];
+					out_alpha[i] = in_alpha[i];
+				}
+				else
+				{
+					out_red[i] = out_green[i] = out_blue[i] = 0;
+					out_alpha[i] = 255; 
+				}
+			}
+			else
+			{
+				out_red[i] = out_green[i] = out_blue[i] = 0;
+				out_alpha[i] = 255; 
+			}
+		}
+		else
+		{
+			out_red[i] = out_green[i] = out_blue[i] = 0;
+			out_alpha[i] = 255; 
+		}
+	}	     
 }
