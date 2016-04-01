76529bbf9f8f7b652cc9717cfd95828469c640c3
abhagwt2 <abhagwt2@0c76c217-e028-4356-9bb7-d03e9a460cca>
2016-04-01 17:58:41 +0000

    Final Submission

--- lab9/vector.c
+++ lab9/vector.c
@@ -4,25 +4,51 @@
 
 vector_t * createVector(int initialSize)
 {
+	vector_t* my_vector = (vector_t*) malloc(sizeof(vector_t));
+	my_vector->maxSize = initialSize; 
+	my_vector->size = 0;
+	my_vector->array = malloc(sizeof(int)*initialSize);
+	return my_vector;
 }
 
 void destroyVector(vector_t * vector)
 {
+	free(vector->array);
+	free(vector);
 }
 
 void resize(vector_t * vector)
 {
+	vector->maxSize = 2*vector->maxSize;
+	vector->array = realloc(vector->array, sizeof(int)*vector->maxSize);
 }
 
 void push_back(vector_t * vector, int element)
 {
+	if(vector->size == vector->maxSize)
+	{
+		resize(vector);
+	}
+	vector->size = vector->size+1;
+	vector->array[vector->size-1] = element;
 }
 
 int pop_back(vector_t * vector)
 {
+	if(vector->size == 0)
+	{
+		return 0;
+	}
+	vector->size = vector->size-1;
+	return vector->array[vector->size];
 }
 
 int access(vector_t * vector, int index)
 {
+	if(index < 0 || index > vector->size)
+	{
+		return 0;
+	}
+	return vector->array[index];
 }
 
