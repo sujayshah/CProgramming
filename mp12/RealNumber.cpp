5e4f37616a88597e6da8ffd1f0512ade21413b1d
kemmere2 <kemmere2@0c76c217-e028-4356-9bb7-d03e9a460cca>
2016-04-22 02:42:20 +0000

    Uploading mp12

---
+++ mp12/RealNumber.cpp
@@ -0,0 +1,9 @@
+#include "RealNumber.h"
+
+//to_String converts real and imaginary components to string of type a+bi
+string RealNumber::to_String(void){
+	stringstream my_output;
+	my_output << value;
+	return my_output.str();
+}
+
